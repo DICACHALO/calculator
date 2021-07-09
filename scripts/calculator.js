0829e68d98357d39041a05bf0bde9c79f68c0e26
Diana Chacon <dchacon871@unab.edu.co>
2021-07-09 10:40:48 -0500

    functional numbers

---
+++ scripts/calculator.js
@@ -0,0 +1,78 @@
+let text = document.getElementById("text");
+
+let btn1 = document.getElementById("btn1");
+let btn2 = document.getElementById("btn2");
+let btn3 = document.getElementById("btn3");
+let btn4 = document.getElementById("btn4");
+let btn5 = document.getElementById("btn5");
+let btn6 = document.getElementById("btn6");
+let btn7 = document.getElementById("btn7");
+let btn8 = document.getElementById("btn8");
+let btn9 = document.getElementById("btn9");
+let btn0 = document.getElementById("btn0");
+
+
+const fn1 = () => {
+    text.innerHTML += "1";
+  };
+  const fn2 = () => {
+    text.innerHTML += "2";
+  };
+  const fn3 = () => {
+    text.innerHTML += "3";
+  };
+  const fn4 = () => {
+    text.innerHTML += "4";
+  };
+  const fn5 = () => {
+    text.innerHTML += "5";
+  };
+  const fn6 = () => {
+    text.innerHTML += "6";
+  };
+  const fn7 = () => {
+    text.innerHTML += "7";
+  };
+  const fn8 = () => {
+    text.innerHTML += "8";
+  };
+  const fn9 = () => {
+    text.innerHTML += "9";
+  };
+  const fn0 = () => {
+    text.innerHTML += "0";
+  };
+
+
+// Events
+
+btn1.onclick = function () {
+    fn1();
+  };
+  btn2.onclick = function () {
+    fn2();
+  };
+  btn3.onclick = function () {
+    fn3();
+  };
+  btn4.onclick = function () {
+    fn4();
+  };
+  btn5.onclick = function () {
+    fn5();
+  };
+  btn6.onclick = function () {
+    fn6();
+  };
+  btn7.onclick = function () {
+    fn7();
+  };
+  btn8.onclick = function () {
+    fn8();
+  };
+  btn9.onclick = function () {
+    fn9();
+  };
+  btn0.onclick = function () {
+    fn0();
+  };  
